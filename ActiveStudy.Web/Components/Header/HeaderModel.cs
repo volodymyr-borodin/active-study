6fd98b4c0b72bc900457f392fd6b9a6fd304d846
vladimirborodin <boberneprotiv@gmail.com>
2021-11-10 21:06:49 +0200

    Clear-up html

--- ActiveStudy.Web/Components/Header/HeaderModel.cs
+++ ActiveStudy.Web/Components/Header/HeaderModel.cs
@@ -2,11 +2,13 @@ namespace ActiveStudy.Web.Components.Header
 {
     public class HeaderModel
     {
-        public HeaderModel(bool isAuthenticated)
+        public HeaderModel(bool isAuthenticated, AccountModel accountModel)
         {
             IsAuthenticated = isAuthenticated;
+            AccountModel = accountModel;
         }
 
         public bool IsAuthenticated { get; }
+        public AccountModel AccountModel { get; }
     }
 }
