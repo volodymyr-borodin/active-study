df961073718d4dbdc49fa11c4e0c66d90c128366
vladimirborodin <boberneprotiv@gmail.com>
2021-11-13 17:21:13 +0200

    Add student pagination

--- ActiveStudy.Web/Models/Students/CreateStudentViewModel.cs
+++ ActiveStudy.Web/Models/Students/CreateStudentViewModel.cs
@@ -1,15 +1,18 @@
 using ActiveStudy.Domain.Crm;
+using ActiveStudy.Domain.Crm.Schools;
 
 namespace ActiveStudy.Web.Models.Students
 {
     public class CreateStudentViewModel : CreateStudentInputModel
     {
-        public CreateStudentViewModel(ClassShortInfo @class = null)
+        public CreateStudentViewModel(School school, ClassShortInfo @class)
         {
+            School = school;
             Class = @class;
             ClassId = @class?.Id;
         }
 
+        public School School { get; }
         public ClassShortInfo Class { get; }
     }
 }
