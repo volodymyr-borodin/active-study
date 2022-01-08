bdc8e7d493cabe463b7e5dc23eb016cfc9b4b5a9
vladimirborodin <boberneprotiv@gmail.com>
2022-01-08 11:35:27 +0200

    Add api project

---
+++ ActiveStudy.Api/Controllers/EducationMaterials/FlashCardsController.cs
@@ -0,0 +1,32 @@
+using ActiveStudy.Domain.Materials.FlashCards;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ActiveStudy.Api.Controllers.EducationMaterials;
+
+[ApiController]
+[Route("education-materials/flash-cards")]
+public class FlashCardsController : ControllerBase
+{
+    private readonly FlashCardsService flashCardsService;
+
+    public FlashCardsController(FlashCardsService flashCardsService)
+    {
+        this.flashCardsService = flashCardsService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var items = await flashCardsService.FindAsync();
+
+        return Ok(items);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Details(string id)
+    {
+        var item = await flashCardsService.GetByIdAsync(id);
+
+        return Ok(item);
+    }
+}
---
+++ ActiveStudy.Api/Startup.cs
@@ -0,0 +1,59 @@
+using ActiveStudy.Domain.Materials.FlashCards;
+using ActiveStudy.Storage.Mongo.Materials;
+using ActiveStudy.Storage.Mongo.Materials.FlashCards;
+using Microsoft.OpenApi.Models;
+using MongoDB.Driver;
+
+namespace ActiveStudy.Api
+{
+    public class Startup
+    {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            services.AddHttpContextAccessor();
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "ActiveStudy API",
+                    Version = "1.0.0"
+                });
+            });
+
+            var mongoUrl = MongoUrl.Create(configuration["MONGO_CONNECTION"]);
+
+            services.AddScoped(_ => new MaterialsContext(mongoUrl));
+            services.AddScoped<IFlashCardsStorage, FlashCardsStorage>();
+            services.AddScoped<FlashCardsService>();
+
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app
+                .UseSwagger()
+                .UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "ActiveStudy V1");
+                });
+
+            app.UseRouting();
+
+            app.UseCors(builder => builder
+                .AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod());
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+    }
+}
