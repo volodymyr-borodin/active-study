f1daf8d5c10918dfdef2f2c2d571ebed3433b70c
vladimirborodin <boberneprotiv@gmail.com>
2022-01-14 20:40:53 +0200

    Improve answer check

--- ActiveStudy.Domain/Materials/FlashCards/Progress/AnswerResult.cs
+++ ActiveStudy.Domain/Materials/FlashCards/Progress/AnswerResult.cs
@@ -1,8 +1,36 @@
 using System;
+using System.Linq;
 
 namespace ActiveStudy.Domain.Materials.FlashCards.Progress;
 
 public record AnswerResult(NewAnswer Answer, FlashCard Card)
 {
-    public bool IsCorrect => Card.Term.Equals(Answer.Answer.Trim(), StringComparison.InvariantCultureIgnoreCase);
+    public bool IsCorrect => CompareSentences(StringToClearSentence(Card.Term), StringToClearSentence(Answer.Answer));
+
+    private static bool CompareSentences(string[] a, string[] b)
+    {
+        if (a.Length != b.Length)
+        {
+            return false;
+        }
+
+        for (var i = 0; i < a.Length; i++)
+        {
+            if (!a[i].Equals(b[i], StringComparison.InvariantCultureIgnoreCase))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static string[] StringToClearSentence(string t)
+    {
+        return t.Split('/')
+            .Select(a => string.Join(' ', a.Trim().Split(' ')
+                .Where(w => w != "a" && w != "an" && w != "the" && w != "to")))
+            .OrderBy(a => a)
+            .ToArray();
+    }
 }
