76d6ae8cfa490e4a9996ff2743bcbea2bbc40fcb
volodymyr.borodin <boberneprotiv@gmail.com>
2022-07-24 10:46:46 +0300

    Add front theme

--- ActiveStudy.Web/Models/Account/LoginInputModel.cs
+++ ActiveStudy.Web/Models/Account/LoginInputModel.cs
@@ -5,9 +5,13 @@ namespace ActiveStudy.Web.Models.Account
     public class LoginInputModel
     {
         [Required]
+        [DataType(DataType.EmailAddress)]
         public string Username { get; set; }
+
         [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
         public bool RememberLogin { get; set; }
     }
 }
